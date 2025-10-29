715c47405cc88f3cb6d53de284fb530d26a082d0
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-10-29 09:38:53 +0100

    Add I2C components check on Slave startup

--- Firmwares/Slave/LMS-S1-G2/slave/slave.ino
+++ Firmwares/Slave/LMS-S1-G2/slave/slave.ino
@@ -1,12 +1,64 @@
 #include "protocol.h"
 #include "espnow_handler.h"
+#include "at24c02.h"
+#include "lm75a.h"
 #include <esp_wifi.h>
+
+#define I2C_SDA_PIN 18
+#define I2C_SCL_PIN 19
+
+AT24C02 eeprom(AT24C02_ADDR);
+LM75A tempPower(LM75A_ADDR_POWER);
+LM75A tempLed1(LM75A_ADDR_LED1);
+LM75A tempLed2(LM75A_ADDR_LED2);
+
+bool checkComponents() {
+  bool allOk = true;
+  
+  Serial.println("Checking I2C components...");
+  
+  if (!eeprom.begin(I2C_SDA_PIN, I2C_SCL_PIN)) {
+    Serial.println("ERROR: AT24C02 EEPROM (0x50) not found");
+    allOk = false;
+  } else {
+    Serial.println("OK: AT24C02 EEPROM (0x50) connected");
+  }
+  
+  if (!tempPower.begin(I2C_SDA_PIN, I2C_SCL_PIN)) {
+    Serial.println("ERROR: LM75A Power (0x4C) not found");
+    allOk = false;
+  } else {
+    Serial.println("OK: LM75A Power (0x4C) connected");
+  }
+  
+  if (!tempLed1.begin(I2C_SDA_PIN, I2C_SCL_PIN)) {
+    Serial.println("ERROR: LM75A LED1 (0x48) not found");
+    allOk = false;
+  } else {
+    Serial.println("OK: LM75A LED1 (0x48) connected");
+  }
+  
+  if (!tempLed2.begin(I2C_SDA_PIN, I2C_SCL_PIN)) {
+    Serial.println("ERROR: LM75A LED2 (0x4E) not found");
+    allOk = false;
+  } else {
+    Serial.println("OK: LM75A LED2 (0x4E) connected");
+  }
+  
+  return allOk;
+}
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   
   Serial.println("LumiRail Slave - Starting...");
+  
+  if (!checkComponents()) {
+    Serial.println("WARNING: Some components are missing. Continuing anyway...");
+  } else {
+    Serial.println("All I2C components detected successfully");
+  }
   
   espnowHandler.init();
   
