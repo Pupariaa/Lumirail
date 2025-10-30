a8a9b29c8faa0b1ad74bcf3e068ee78ab0f677c6
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-10-30 15:55:45 +0100

    Master: include AP IP in FW_BEGIN; stable FW_ACK logging; don't start AP here; support 188B chunks; suspend scan during update

--- Firmwares/Master/master/espnow_handler.cpp
+++ Firmwares/Master/master/espnow_handler.cpp
@@ -5,11 +5,10 @@
 
 EspNowHandler espnowHandler;
 
-EspNowHandler::EspNowHandler() : sequenceCounter(0), lastBroadcast(0), broadcastPeerAdded(false) {}
+EspNowHandler::EspNowHandler() : sequenceCounter(0), lastBroadcast(0), broadcastPeerAdded(false), fwActive(false) {}
 
 void EspNowHandler::init() {
-  WiFi.mode(WIFI_AP_STA);
-  WiFi.softAP("LumiRail", NULL, 1, 0, 1);
+  // WiFi AP is started in setup()
   
   if (esp_now_init() != ESP_OK) {
     Serial.println("ERROR: Failed to initialize ESP-NOW");
@@ -33,7 +32,7 @@ void EspNowHandler::update() {
 void EspNowHandler::sendBroadcastDiscovery() {
   if (!broadcastPeerAdded) {
     uint8_t broadcastAddr[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-    esp_now_peer_info_t peerInfo;
+    esp_now_peer_info_t peerInfo; memset(&peerInfo, 0, sizeof(peerInfo));
     memcpy(peerInfo.peer_addr, broadcastAddr, 6);
     peerInfo.channel = 1;
     peerInfo.ifidx = WIFI_IF_AP;
@@ -68,7 +67,7 @@ bool EspNowHandler::sendPing(const uint8_t* mac) {
   msg.payloadLength = 0;
   msg.checksum = calculateChecksum(msg);
   
-  esp_now_peer_info_t peerInfo;
+  esp_now_peer_info_t peerInfo; memset(&peerInfo, 0, sizeof(peerInfo));
   memcpy(peerInfo.peer_addr, mac, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
@@ -98,7 +97,7 @@ bool EspNowHandler::sendPairRequest(const uint8_t* mac) {
   msg.payloadLength = 0;
   msg.checksum = calculateChecksum(msg);
   
-  esp_now_peer_info_t peerInfo;
+  esp_now_peer_info_t peerInfo; memset(&peerInfo, 0, sizeof(peerInfo));
   memcpy(peerInfo.peer_addr, mac, 6);
   peerInfo.channel = 1;
   peerInfo.ifidx = WIFI_IF_AP;
@@ -137,7 +136,7 @@ bool EspNowHandler::sendUnpair(const uint8_t* mac) {
   msg.payloadLength = 0;
   msg.checksum = calculateChecksum(msg);
   
-  esp_now_peer_info_t peerInfo;
+  esp_now_peer_info_t peerInfo; memset(&peerInfo, 0, sizeof(peerInfo));
   memcpy(peerInfo.peer_addr, mac, 6);
   peerInfo.channel = 1;
   peerInfo.ifidx = WIFI_IF_AP;
@@ -177,7 +176,7 @@ bool EspNowHandler::sendCommand(const uint8_t* mac, const char* command) {
   msg.payloadLength = strlen(command);
   msg.checksum = calculateChecksum(msg);
   
-  esp_now_peer_info_t peerInfo;
+  esp_now_peer_info_t peerInfo; memset(&peerInfo, 0, sizeof(peerInfo));
   memcpy(peerInfo.peer_addr, mac, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
@@ -199,7 +198,7 @@ bool EspNowHandler::sendCommand(const uint8_t* mac, const char* command) {
 bool EspNowHandler::sendBroadcastCommand(const char* command) {
   if (!broadcastPeerAdded) {
     uint8_t broadcastAddr[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-    esp_now_peer_info_t peerInfo;
+    esp_now_peer_info_t peerInfo; memset(&peerInfo, 0, sizeof(peerInfo));
     memcpy(peerInfo.peer_addr, broadcastAddr, 6);
     peerInfo.channel = 1;
     peerInfo.ifidx = WIFI_IF_AP;
@@ -229,6 +228,77 @@ bool EspNowHandler::sendBroadcastCommand(const char* command) {
     Serial.println(esp_err_to_name(result));
     return false;
   }
+}
+
+bool EspNowHandler::sendFwBegin(const uint8_t* mac, uint16_t sessionId, const char version[8], uint32_t sizeBytes, uint32_t crc32) {
+  fwActive = true;
+  EspNowMessage msg;
+  msg.version = 1;
+  msg.type = MSG_FW_BEGIN;
+  msg.sequence = sequenceCounter++;
+  uint8_t *p = msg.payload;
+  p[0] = sessionId & 0xFF; p[1] = (sessionId >> 8) & 0xFF;
+  memcpy(p + 2, version, 8);
+  p[10] = sizeBytes & 0xFF; p[11] = (sizeBytes >> 8) & 0xFF; p[12] = (sizeBytes >> 16) & 0xFF; p[13] = (sizeBytes >> 24) & 0xFF;
+  p[14] = crc32 & 0xFF; p[15] = (crc32 >> 8) & 0xFF; p[16] = (crc32 >> 16) & 0xFF; p[17] = (crc32 >> 24) & 0xFF;
+  extern IPAddress apIP;
+  p[18] = apIP[0]; p[19] = apIP[1]; p[20] = apIP[2]; p[21] = apIP[3];
+  msg.payloadLength = 22;
+  msg.checksum = calculateChecksum(msg);
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, mac, 6);
+  peerInfo.channel = 1;
+  peerInfo.ifidx = WIFI_IF_AP;
+  peerInfo.encrypt = false;
+  esp_now_add_peer(&peerInfo);
+  esp_err_t res = esp_now_send(mac, (uint8_t *)&msg, sizeof(EspNowMessage));
+  esp_now_del_peer(mac);
+  return (res == ESP_OK || res == ESP_ERR_ESPNOW_IF);
+}
+
+bool EspNowHandler::sendFwChunk(const uint8_t* mac, uint16_t sessionId, uint32_t offset, const uint8_t* data, uint16_t len, uint32_t chunkCrc32) {
+  if (len > 188) len = 188;
+  EspNowMessage msg;
+  msg.version = 1;
+  msg.type = MSG_FW_CHUNK;
+  msg.sequence = sequenceCounter++;
+  uint8_t *p = msg.payload;
+  p[0] = sessionId & 0xFF; p[1] = (sessionId >> 8) & 0xFF;
+  p[2] = offset & 0xFF; p[3] = (offset >> 8) & 0xFF; p[4] = (offset >> 16) & 0xFF; p[5] = (offset >> 24) & 0xFF;
+  p[6] = len & 0xFF; p[7] = (len >> 8) & 0xFF;
+  memcpy(p + 8, data, len);
+  p[8 + len] = chunkCrc32 & 0xFF; p[9 + len] = (chunkCrc32 >> 8) & 0xFF; p[10 + len] = (chunkCrc32 >> 16) & 0xFF; p[11 + len] = (chunkCrc32 >> 24) & 0xFF;
+  msg.payloadLength = 12 + len;
+  msg.checksum = calculateChecksum(msg);
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, mac, 6);
+  peerInfo.channel = 1;
+  peerInfo.ifidx = WIFI_IF_AP;
+  peerInfo.encrypt = false;
+  esp_now_add_peer(&peerInfo);
+  esp_err_t res = esp_now_send(mac, (uint8_t *)&msg, sizeof(EspNowMessage));
+  esp_now_del_peer(mac);
+  return (res == ESP_OK || res == ESP_ERR_ESPNOW_IF);
+}
+
+bool EspNowHandler::sendFwEnd(const uint8_t* mac, uint16_t sessionId) {
+  EspNowMessage msg;
+  msg.version = 1;
+  msg.type = MSG_FW_END;
+  msg.sequence = sequenceCounter++;
+  msg.payload[0] = sessionId & 0xFF; msg.payload[1] = (sessionId >> 8) & 0xFF;
+  msg.payloadLength = 2;
+  msg.checksum = calculateChecksum(msg);
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, mac, 6);
+  peerInfo.channel = 1;
+  peerInfo.ifidx = WIFI_IF_AP;
+  peerInfo.encrypt = false;
+  esp_now_add_peer(&peerInfo);
+  esp_err_t res = esp_now_send(mac, (uint8_t *)&msg, sizeof(EspNowMessage));
+  esp_now_del_peer(mac);
+  fwActive = false;
+  return (res == ESP_OK || res == ESP_ERR_ESPNOW_IF);
 }
 
 void EspNowHandler::onDataSent(const esp_now_send_info_t *info, esp_now_send_status_t status) {
@@ -268,6 +338,16 @@ void EspNowHandler::onDataReceive(const esp_now_recv_info_t *info, const uint8_t
       break;
     case MSG_PAIR_RESPONSE:
       slaveManager.handlePairResponse(mac_addr, msg);
+      break;
+    case MSG_FW_ACK:
+      {
+        uint16_t sid = (uint16_t)(msg->payload[0] | (msg->payload[1] << 8));
+        uint32_t off = (uint32_t)msg->payload[2] | ((uint32_t)msg->payload[3] << 8) | ((uint32_t)msg->payload[4] << 16) | ((uint32_t)msg->payload[5] << 24);
+        Serial.printf("FW_ACK session=%u nextOffset=%u\n", sid, off);
+      }
+      break;
+    case MSG_FW_ERROR:
+      Serial.println("FW_ERROR received");
       break;
   }
 }
