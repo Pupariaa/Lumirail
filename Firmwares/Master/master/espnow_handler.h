9d84e9bdf425fd719e759c43cfaedb78ff24e18a
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-10-30 15:55:32 +0100

    Master: add FW update APIs and fwActive flag in espnow handler header

--- Firmwares/Master/master/espnow_handler.h
+++ Firmwares/Master/master/espnow_handler.h
@@ -15,6 +15,11 @@ public:
   bool sendUnpair(const uint8_t* mac);
   bool sendCommand(const uint8_t* mac, const char* command);
   bool sendBroadcastCommand(const char* command);
+  bool sendFwBegin(const uint8_t* mac, uint16_t sessionId, const char version[8], uint32_t sizeBytes, uint32_t crc32);
+  bool sendFwChunk(const uint8_t* mac, uint16_t sessionId, uint32_t offset, const uint8_t* data, uint16_t len, uint32_t chunkCrc32);
+  bool sendFwEnd(const uint8_t* mac, uint16_t sessionId);
+  bool isFwActive() const { return fwActive; }
+  void setFwActive(bool v) { fwActive = v; }
   
   uint16_t getSequenceCounter() { return sequenceCounter; }
   
@@ -26,6 +31,7 @@ private:
   void sendBroadcastDiscovery();
   static void onDataSent(const esp_now_send_info_t *info, esp_now_send_status_t status);
   static void onDataReceive(const esp_now_recv_info_t *info, const uint8_t *data, int len);
+  bool fwActive;
 };
 
 extern EspNowHandler espnowHandler;
