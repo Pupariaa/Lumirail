8f4fcb46aaae8b2f49b145a73b551d91b19538c4
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-12-25 06:13:34 +0100

    Add webpack.preload.config.js: webpack config for preload script

---
+++ Software/Application/Lumirail Studio/webpack.preload.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  target: 'electron-preload',
+  entry: './src/preload/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist/preload'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+};
+
