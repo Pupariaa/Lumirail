f91b61ba2c85c161b7137a0a3396559a8433fa58
Maxime <32194766+Pupariaa@users.noreply.github.com>
2026-02-02 00:40:15 +0100

    feat(serial): export estimateUploadTimeMs for upload duration display
    - Re-export estimateUploadTimeMs from serial module so UI can display
      estimated time remaining during scene upload

---
+++ Software/Web-Application/src/serial/index.ts
@@ -0,0 +1,4 @@
+export { serialConnection, estimateUploadTimeMs } from './serialConnection'
+export type { ModuleInfo, UploadSceneOptions } from './serialConnection'
+export { isValidModuleSerial } from './moduleSerial'
+export type { SerialConnectionState, SerialPort } from './types'
