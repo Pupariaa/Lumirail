07cb7d11f648e89b551e2367cea2511813c3c5d7
Maxime <32194766+Pupariaa@users.noreply.github.com>
2026-02-04 04:00:02 +0100

    esp8266: revise storage API for scene binaries
    Update config read/write signatures, expose file copy + scene commit helpers, and add an API to read LFP metadata from the stored scene binary.

--- Firmwares/LMS-L1-ESP8266/storage.h
+++ Firmwares/LMS-L1-ESP8266/storage.h
@@ -4,10 +4,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-void configRead(uint8_t* out_active, uint8_t* out_sop, uint8_t* out_sot);
-bool configWrite(uint8_t active, uint8_t sop, uint8_t sot);
-bool sceneMetaRead(const char* path, uint32_t* out_delay, uint8_t* out_loop);
-bool sceneMetaWrite(const char* path, const char* hash64, uint32_t size, uint32_t delay, uint8_t loop);
+void configRead(uint8_t* out_active, uint8_t* out_sop, uint8_t* out_sot, uint8_t* out_play_loop);
+bool configWrite(uint8_t active, uint8_t sop, uint8_t sot, uint8_t play_loop);
+bool copyFile(const char* src, const char* dst);
+bool sceneCommit(void);
+bool sceneLfpMeta(uint16_t* out_tick_ms, uint32_t* out_frame_count, uint16_t* out_channel_count);
 void printStorageState(void);
 
 #endif
