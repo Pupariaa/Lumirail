253968b0b4c81756823e9c4b232b066adc80ff5a
Maxime <32194766+Pupariaa@users.noreply.github.com>
2026-02-04 03:59:44 +0100

    esp8266: update serial protocol header
    Expose scene meta dump helper and generalize scene dump to accept a path.

--- Firmwares/LMS-L1-ESP8266/serial_protocol.h
+++ Firmwares/LMS-L1-ESP8266/serial_protocol.h
@@ -2,14 +2,16 @@
 #define LMS_L1_ESP8266_SERIAL_PROTOCOL_H
 
 #include <stddef.h>
+#include <stdint.h>
 #include <stdbool.h>
 
 void sendLine(const char* s);
 bool waitForLineAck(const char* ack, size_t ackLen);
 void sendFileLinesWithHash(const char* path, const char* endPrefix, const char* lineAck, size_t lineAckLen);
 void sendFileBlock(const char* path, const char* endPrefix);
+void sendSceneMetaBlock(const char* endPrefix);
 void sendFileText(const char* path, const char* errLine, const char* endLine);
-void dumpSceneFile(void);
+void dumpSceneFile(const char* path);
 void debugRxHex(const char* buf, size_t n);
 
 #endif
